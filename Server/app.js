d4831858b6af2be216fab24c3306f6678aa401ae
ChanguaZzz <ggualdronarjona@gmail.com>
2025-02-11 17:58:13 +0100

    fix: update CORS configuration to allow requests from production domain

--- Server/app.js
+++ Server/app.js
@@ -36,7 +36,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://hikarisushi.onrender.com"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
